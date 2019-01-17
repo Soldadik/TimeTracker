a75640bba5b97504e10b7607c518b91f2508e5ca
Evgeniy <evg.ifmoitmo@gmail.com>
2019-01-17 15:15:25 +0300

    modified:   TimeTracker/TimerControl.xaml.cs

--- TimeTracker/TimerControl.xaml.cs
+++ TimeTracker/TimerControl.xaml.cs
@@ -1,6 +1,8 @@
 ﻿namespace TimeTracker
 {
+    using System;
     using System.Diagnostics.CodeAnalysis;
+    using System.IO;
     using System.Windows;
     using System.Windows.Controls;
 
@@ -9,12 +11,40 @@
     /// </summary>
     public partial class TimerControl : UserControl
     {
+        System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+        string time_total;
+        int i = 0;
+        DateTime date = new DateTime();
+        DateTime date_total = new DateTime();
+
         /// <summary>
         /// Initializes a new instance of the <see cref="TimerControl"/> class.
         /// </summary>
         public TimerControl()
         {
             this.InitializeComponent();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            i++;
+            textBlock_current.Text = i.ToString();
+        }
+
+        private void ReadInfoFromFile()
+        {
+            StreamReader ReadFile = File.OpenText("time_tracker.cfg");
+            ReadFile.Close();
+
+        }
+
+        private void WriteInfoToFile()
+        {
+            StreamWriter WriteFile = new StreamWriter("time_tracker.cfg");
+            WriteFile.Close();
         }
 
         /// <summary>
@@ -27,12 +57,12 @@
 
         private void button_start_Click(object sender, RoutedEventArgs e)
         {
-
+            dispatcherTimer.Start();
         }
 
         private void button_stop_Click(object sender, RoutedEventArgs e)
         {
-
+            dispatcherTimer.Stop();
         }
     }
 }
