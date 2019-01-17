c419102b9f3a39f9c99f51b65b207f1769df9146
Evgeniy <evg.ifmoitmo@gmail.com>
2019-01-17 15:09:59 +0300

    new file:   TimeTracker/Key.snk 	new file:   TimeTracker/Resources/TimerCommand.png 	new file:   TimeTracker/Resources/TimerPackage.ico 	modified:   TimeTracker/TimeTracker.csproj 	new file:   TimeTracker/Timer.cs 	new file:   TimeTracker/TimerCommand.cs 	new file:   TimeTracker/TimerControl.xaml 	new file:   TimeTracker/TimerControl.xaml.cs 	new file:   TimeTracker/TimerPackage.cs 	new file:   TimeTracker/TimerPackage.vsct 	new file:   TimeTracker/VSPackage.resx 	new file:   TimeTracker/packages.config 	modified:   TimeTracker/source.extension.vsixmanifest 	TimeTracker.sln

---
+++ TimeTracker/TimerControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace TimeTracker
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for TimerControl.
+    /// </summary>
+    public partial class TimerControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TimerControl"/> class.
+        /// </summary>
+        public TimerControl()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles click on the button by displaying a message box.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event args.</param>
+        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
+
+        private void button_start_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_stop_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
